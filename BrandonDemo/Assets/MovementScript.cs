7f751dacf1f0d09f71e254c03f27c9db406bee1e
Brandon <bborders1@mail.csuchico.edu>
2017-01-29 00:13:59 -0800

    Added Demo to git repo

---
+++ BrandonDemo/Assets/MovementScript.cs
@@ -0,0 +1,32 @@
+﻿//This script was written by Brandon | Last edited by Brandon | Modified on Jan 28, 2017
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+    
+    //Player speed
+    public int speed = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Movement when the player presses an arrow key. 
+	    if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+    }
+
+
+}
